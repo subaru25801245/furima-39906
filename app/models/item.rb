d0af75d6267fecedfb3bd817c706ddf0f2c7afe9
subaru25801245 <subaru25801245@gmail.com>
2023-09-27 17:39:02 +0900

    商品の詳細と配送についてのプルダウン実装。画像取り込みは途中。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_burden, class_name: 'DeliveryBurden'
+  belongs_to :prefecture
+  belongs_to :delivery_day, class_name: 'DeliveryDay'
+
+  belongs_to :user
 end
