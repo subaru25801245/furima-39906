9999098ce285c3deda059424acc5e82aa0c1a93b
subaru25801245 <subaru25801245@gmail.com>
2023-09-30 18:34:46 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update]
+  before_action :check_user, only: [:edit, :update]
   
   def index
     @items = Item.all.order(created_at: :desc)
@@ -21,11 +23,32 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
   
 
   private
   def item_params
     params.require(:item).permit(:name, :info, :image, :category_id, :condition_id, :del_burden_id, :prefecture_id, :del_day_id, :price).merge(user_id:current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_user
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
 end
